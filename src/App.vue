817ccdf108736e2b0d1877380fcf25654019e82c
chaauCindy <chaau84@gmail.com>
2017-07-31 18:26:52 +0900

    환경 설정

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  #app
+    header
+    main
+    footer
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  @import "~default";
+
+  html
+    font-size: 100%
+    background: #fff
+  body
+    margin: 0
+</style>
