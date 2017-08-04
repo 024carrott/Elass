3f2f6c2a2a4ed992cb281c1d21bb8e24cc3c6b25
chaauCindy <chaau84@gmail.com>
2017-08-04 14:50:53 +0900

    강의 상세정보 페이지 마크업 추가

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App';
 import List from './html/list';
+import View from './html/view';
+import Mypage from './html/mypage';
 
 new Vue({
   el: '#app',
-  render: h => h(List)
+  render: h => h(Mypage)
 });
