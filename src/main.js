8e5b44bd07dccdb4aad0fea39c48242c85a457ab
KimJiSu <kimjisu@KimJiSuui-MacBook-Pro.local>
2017-08-07 15:14:44 +0900

    강의 등록하기 페이지 마크업 완료

--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@ import SignIn from './html/SignIn';
 import List from './html/list';
 import View from './html/view';
 import Mypage from './html/mypage';
+import mypageRegisterclass from './html/mypage-registerclass';
 
 new Vue({
   el: '#app',
-  render: h => h(List)
+  render: h => h(mypageRegisterclass)
 });
