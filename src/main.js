3ddcc65ae4b59c870ceb13deac4b256ec24b08ce
chaauCindy <chaau84@gmail.com>
2017-08-07 17:54:38 +0900

    vuex test

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 import routes from './routes';
 import App from './App';
  
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 const router = new VueRouter({
   mode: 'history',
